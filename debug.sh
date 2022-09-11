41271d0595c31cd750a58499ec22e2f81d0cd78b
sabudilovskiy <sabudilovskiy@yandex.ru>
2022-09-11 08:18:51 -0400

    Set up conan and cmake

---
+++ debug.sh
@@ -0,0 +1,1 @@
+conan install . --build=missing --install-folder conan_libraries --profile debug
---
+++ hello-build/CMakeFiles/RestInoBackend.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RestInoBackend.dir/main.cpp.o"
+  "RestInoBackend"
+  "RestInoBackend.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RestInoBackend.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
