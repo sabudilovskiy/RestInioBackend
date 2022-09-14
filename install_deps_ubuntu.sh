7c35554e61b1e96ca50501833d70fdf5db7ea663
sabudilovskiy <sabudilovskiy@yandex.ru>
2022-09-14 16:15:00 +0300

    Обновлён скрипт установки

--- install_deps_ubuntu.sh
+++
@@ -1,6 +0,0 @@
-wget  -O ./conan.deb "https://github.com/conan-io/conan/releases/latest/download/conan-ubuntu-64.deb"
-sudo dpkg -i conan.deb
-sudo apt install g++
-sudo apt install cmake
-cp -R ./profiles ~/.conan/profiles
-source debug.sh
